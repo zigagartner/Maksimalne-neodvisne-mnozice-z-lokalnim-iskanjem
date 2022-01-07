862dea6063689dbadcdba7225115e9aa15d9f6a5
Jaka mrak <mrak.jaka9@gmail.com>
2022-01-07 18:21:22 +0100

    popravil generiranje grafov z izpisom seznama teh

--- Iskanje_NNM/generiranje-grafov.py
+++ Iskanje_NNM/generiranje-grafov.py
@@ -5,41 +5,41 @@ import matplotlib.pyplot as plt
 import json 
 
 
-def erdos_reyni(n,p,m):
-    dict = {}
+def erdos_reyni(n,p,m,ime_datoteke_json):
+    db_json = {}
+    A = []
     for i in range(m):
         G = nx.erdos_renyi_graph(n,p)
         G_slovar = nx.to_dict_of_lists(G)
-        dict[i] = G_slovar
-    with open('generirani_grafi.json','w') as js:
-        json.dump(dict,js)
+        db_json[i] = G_slovar
+        A += [G_slovar]
+    with open(ime_datoteke_json +'.json','w') as js:
+        json.dump(db_json,js)
+    return A
 
-   
+
+
+
+
+#ne dela vredu ker na koncu ko spreminjam kljuce iz str v int zafrknem in dodam k vsakem klucu isti slovar (treba zanke prau nastavit s+ce bova rabla)   
+#def preberi_json(json_file):
+#    with open(json_file+'.json') as file:
+#            db_json = json.load(file)
+#    output_dict = {}
+#    for key, value in db_json.items():
+#        output_dict[int(key)] = value
+#    koncni = {}
+#    for key,value in output_dict.items():
+#        for k,v in value.items():
+#            koncni[int(k)] = v
+#        output_dict[key] = koncni
+#    return output_dict
 
         
 
-        
 
-
-
-
-
-erdos_reyni(10,0.4,5)
 
 
 #nx.draw(G, with_labels=True)
 #plt.show()
 
-#for i in range(10):
-#    dict = {}
-#    vrednosti = 
-#    kljuci = list(range(10))
-#    G = nx.erdos_renyi_graph(5,0.5)
-#    G_slovar = nx.to_dict_of_lists(G)
-#
-#    dict = dict.fromkeys() G_slovar
-#    print(dict)
-#
-
-
-    
